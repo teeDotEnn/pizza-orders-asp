3a0ca3ddb48b09af547644bc838ea611859b3ddf
tim <nigh.timothy@gmail.com>
2020-08-30 16:11:07 -0400

    Added the DatabaseSettings Model to the ConfigureServices section

--- PizzaOrders/Startup.cs
+++ PizzaOrders/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using PizzaOrders.Models;
 
 namespace PizzaOrders
 {
@@ -24,6 +26,11 @@ namespace PizzaOrders
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            
+            services.Configure<DatabaseSettings>
+                (Configuration.GetSection(nameof(DatabaseSettings)));
+            services.AddSingleton<IDatabaseSettings>
+                (x=>x.GetRequiredService<IOptions<DatabaseSettings>>().Value);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
