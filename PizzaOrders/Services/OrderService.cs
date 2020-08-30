c77267e19d4accd0dcdbfeec360dd64b4db35f21
tim <nigh.timothy@gmail.com>
2020-08-30 16:28:05 -0400

    Added OrderService.cs Class

---
+++ PizzaOrders/Services/OrderService.cs
@@ -0,0 +1,17 @@
+/* File Name: OrderService.cs
+ * Purpose: To provide CRUD functionality to the
+ *          webapp when it is working in the orders collection
+ * Rev History:
+ *     Created Aug 2020 by Tim Nigh
+ */
+using System.Collections.Generic;
+using MongoDB.Driver;
+using PizzaOrders.Models;
+
+namespace PizzaOrders.Services
+{
+    public class OrderService
+    {
+        
+    }
+}
